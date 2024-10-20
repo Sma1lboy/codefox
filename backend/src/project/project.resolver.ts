daf719ad94c65330a1685d78bf4d7cc1607498fb
ZHallen122 <zhallen12261@gmail.com>
2024-10-19 23:12:13 -0400

    remove unnesseary file

--- backend/src/project/project.resolver.ts
+++ backend/src/project/project.resolver.ts
@@ -8,16 +8,13 @@ import {
     Resolver,
 } from '@nestjs/graphql';
 import { ProjectsService } from './project.service';
-import { ProjectPackagesService } from './project-packages.service';
 import { Projects } from './project.model';
-import { ProjectPackages } from './project-packages.model';
 import { UpsertProjectInput } from './dto/project.input';
 
 @Resolver(() => Projects)
 export class ProjectsResolver {
   constructor(
-    private readonly projectsService: ProjectsService,
-    private readonly projectPackagesService: ProjectPackagesService
+    private readonly projectsService: ProjectsService
   ) {}
 
   // -------- All the code need to extract the user id from the token and verify is the project user's or not
@@ -47,24 +44,6 @@ export class ProjectsResolver {
   @Mutation(() => Boolean)
   async deleteProject(@Args('projectId') projectId: string): Promise<boolean> {
     return this.projectsService.deleteProject(projectId);
-  }
-
-  // @GetAuthToken() token: string
-  @Mutation(() => ProjectPackages)
-  async addPackageToProject(
-    @Args('projectId') projectId: string,
-    @Args('packageContent') packageContent: string
-  ): Promise<ProjectPackages> {
-    return this.projectPackagesService.addPackageToProject(projectId, packageContent);
-  }
-
-  // @GetAuthToken() token: string
-  @Mutation(() => Boolean)
-  async removePackageFromProject(
-    @Args('projectId') projectId: string,
-    @Args('packageId') packageId: string
-  ): Promise<boolean> {
-    return this.projectPackagesService.removePackageFromProject(projectId, packageId);
   }
 
   // @GetAuthToken() token: string
