7635a8a465cba619b387f391a5ddff01b759ed32
NarwhalChen <spixel.xi@gmail.com>
2024-10-31 15:25:27 -0500

    feat(backend): backend config convention

---
+++ backend/src/config/config.service.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import _ from 'lodash';
+
+export class ConfigService{
+    private config: any;
+
+    private readonly configPath: string;
+
+    constructor() {
+        this.configPath = path.resolve(__dirname, "config.json");
+        this.loadConfig();
+    }
+
+    private loadConfig() {
+        const file = fs.readFileSync(this.configPath, "utf-8");
+        this.config = JSON.parse(file);
+    }
+
+     get(path: string) {
+        return _.get(this.config, path);
+    }
+
+    set(path: string, value: any) {
+        _.set(this.config, path, value);
+        this.saveConfig();
+    }
+
+    private saveConfig() {
+        fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 4), "utf-8");
+    }
+}
