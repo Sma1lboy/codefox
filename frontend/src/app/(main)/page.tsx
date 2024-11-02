92e5065a9a2f5a6746e31c00b8162bcd992195e9
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-11-02 17:00:58 -0500

    Feat optimzation render speed (#39)
    - optimiztion speed
    - prevent see anything in auth provider 
    
    
    https://github.com/user-attachments/assets/d3f4e2e5-1147-43e3-9b04-1af703979aed
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Introduced `LoadingPage` component for a full-screen loading overlay
    with theme adaptability.
    - Added `SideBarItem` component for managing chat functionalities in the
    sidebar.
    - New `ScrollArea` and `ScrollBar` components for customizable scroll
    areas.
    - Introduced `Separator` component for rendering horizontal and vertical
    separators.
    	- Added `AuthProvider` for managing user authentication state.
    	- New `BaseProviders` component to wrap essential context providers.
    
    - **Enhancements**
    	- Improved `ChatList` component with message editing capabilities.
    	- Enhanced `ChatTopbar` for a simplified model selection interface.
    - Refactored `Sidebar` to utilize props for chat management, improving
    performance.
    
    - **Bug Fixes**
    	- Fixed styling and transition effects on the `LoginPage` component.
      
    - **Chores**
    - Updated various package dependencies for improved functionality and
    performance.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ frontend/src/app/(main)/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRef, useState } from 'react';
+import { Message } from '@/components/types';
+import { useModels } from '../hooks/useModels';
+import ChatContent from '@/components/chat/chat';
+import { useChatStream } from '../hooks/useChatStream';
+
+export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState('');
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const { selectedModel, setSelectedModel } = useModels();
+
+  const { loadingSubmit, handleSubmit, handleInputChange, stop, isStreaming } =
+    useChatStream({
+      chatId: '',
+      input,
+      setInput,
+      setMessages,
+      selectedModel,
+    });
+
+  return (
+    <ChatContent
+      chatId=""
+      setSelectedModel={setSelectedModel}
+      messages={messages}
+      input={input}
+      handleInputChange={handleInputChange}
+      handleSubmit={handleSubmit}
+      loadingSubmit={loadingSubmit}
+      stop={stop}
+      formRef={formRef}
+      setInput={setInput}
+      setMessages={setMessages}
+    />
+  );
+}
