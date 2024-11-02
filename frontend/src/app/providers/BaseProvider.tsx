85752b078e1d91e1ae6327894d35e67df8be50b8
Jackson Chen <541898146chen@gmail.com>
2024-11-02 16:27:11 -0500

    refactor: Update import paths for RootProvider and AuthProvider

--- frontend/src/app/providers/BaseProvider.tsx
+++ frontend/src/app/providers/BaseProvider.tsx
@@ -4,7 +4,7 @@ import client from '@/lib/client';
 import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
-import { AuthProvider } from '../AuthProvider';
+import { AuthProvider } from './AuthProvider';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -13,13 +13,13 @@ interface ProvidersProps {
 // Base Provider for the app
 export function BaseProviders({ children }: ProvidersProps) {
   return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <ApolloProvider client={client}>
+        <AuthProvider>
           {children}
           <Toaster position="top-right" />
-        </ThemeProvider>
-      </AuthProvider>
-    </ApolloProvider>
+        </AuthProvider>
+      </ApolloProvider>
+    </ThemeProvider>
   );
 }
