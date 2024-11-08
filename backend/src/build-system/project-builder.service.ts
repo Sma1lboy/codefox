7eceda66d2dd27674710eedf9995d46bfed42ea3
Jackson Chen <541898146chen@gmail.com>
2024-11-08 04:32:50 -0600

    feat: Add ModelProvider to ProjectBuilderService
    This commit adds the `ModelProvider` to the `ProjectBuilderService` in the `ProjectBuilderService` file. The `ModelProvider` is used to retrieve an instance of the `ModelProvider` class. This change allows the `ProjectBuilderService` to access the necessary models for creating a new project.

--- backend/src/build-system/project-builder.service.ts
+++ backend/src/build-system/project-builder.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ChatProxyService } from 'src/chat/chat.service';
+import { ModelProvider } from 'src/common/model-provider';
 
 @Injectable()
 export class ProjectBuilderService {
   private readonly logger = new Logger(ProjectBuilderService.name);
 
+  private models: ModelProvider = ModelProvider.getInstance();
   constructor(private chatProxyService: ChatProxyService) {}
 
   async createProject(input: {
