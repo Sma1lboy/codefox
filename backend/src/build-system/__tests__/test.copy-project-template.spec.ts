595a868ac9fa23c2ce480ebd7a8e0feebefc63d0
Jackson Chen <541898146chen@gmail.com>
2024-12-16 23:43:34 -0600

    refactor(tests): use getTemplatePath for template directory in copyProjectTemplate test

--- backend/src/build-system/__tests__/test.copy-project-template.spec.ts
+++ backend/src/build-system/__tests__/test.copy-project-template.spec.ts
@@ -1,23 +1,16 @@
 import { copyProjectTemplate } from 'src/build-system/utils/files';
-import * as path from 'path';
 import { promises as fs } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
+import { getTemplatePath } from 'src/config/common-path';
+import { Logger } from '@nestjs/common';
 
 describe('Copy Project Template', () => {
   it('should copy the template to the specified UUID folder', async () => {
-    const templatePath = path.join(
-      __dirname,
-      '../../../template/template-backend',
-    );
+    const templatePath = getTemplatePath('template-backend');
     const projectUUID = uuidv4();
-    console.log(templatePath);
-
+    Logger.log('template-path:', templatePath);
     const projectPath = await copyProjectTemplate(templatePath, projectUUID);
-
-    // Validate that files were copied
     expect(await fs.access(projectPath)).toBeUndefined(); // Project folder exists
-
-    // Cleanup
     await fs.rm(projectPath, { recursive: true, force: true });
   });
 });
