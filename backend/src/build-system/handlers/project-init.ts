b40fb98aecff7b98e9b3609e289d32b6c0a3e43d
ZHallen122 <zhallen12261@gmail.com>
2024-12-17 09:47:37 -0500

    add copy template to project init

--- backend/src/build-system/handlers/project-init.ts
+++ backend/src/build-system/handlers/project-init.ts
@@ -1,12 +1,16 @@
 import { BuilderContext } from '../context';
 import { BuildHandlerManager } from '../hanlder-manager';
 import { BuildHandler, BuildResult } from '../types';
+import { v4 as uuidv4 } from 'uuid';
+import { getTemplatePath } from 'src/config/common-path';
+import { copyProjectTemplate } from 'src/build-system/utils/files';
 
 export class ProjectInitHandler implements BuildHandler {
   readonly id = 'op:PROJECT::STATE:SETUP';
 
   async run(context: BuilderContext): Promise<BuildResult> {
     console.log('Setting up project...');
+
     const result = {
       projectName: 'online shoping',
       descreption: 'sell products',
@@ -14,6 +18,26 @@ export class ProjectInitHandler implements BuildHandler {
       path: '/path/to/project',
     };
     context.setGlobalContext('projectConfig', result);
+
+    // copy project template
+    const projectUUID = uuidv4();
+
+    //frontend
+    const frontendTemplatePath = getTemplatePath('react-ts');
+    // backend
+    const backendTemplatePath = getTemplatePath('template-backend');
+    const backendProjectPath = await copyProjectTemplate(
+      backendTemplatePath,
+      projectUUID,
+    );
+
+    if (!backendProjectPath) {
+      return {
+        success: false,
+        error: new Error('Failed to copy project template'),
+      };
+    }
+
     return {
       success: true,
       data: result,
