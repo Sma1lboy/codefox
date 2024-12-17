ba15953d2e2b79caa65b60448da6b0f75196db36
ZHallen122 <zhallen12261@gmail.com>
2024-12-17 17:21:27 -0500

    add logger, and remove copy template

--- backend/src/build-system/handlers/project-init.ts
+++ backend/src/build-system/handlers/project-init.ts
@@ -1,34 +1,14 @@
 import { BuilderContext } from '../context';
 import { BuildHandlerManager } from '../hanlder-manager';
 import { BuildHandler, BuildResult } from '../types';
-import { v4 as uuidv4 } from 'uuid';
-import { getTemplatePath } from 'src/config/common-path';
-import { copyProjectTemplate } from 'src/build-system/utils/files';
+import { Logger } from '@nestjs/common';
 
 export class ProjectInitHandler implements BuildHandler {
   readonly id = 'op:PROJECT::STATE:SETUP';
+  private readonly logger = new Logger('ProjectInitHandler');
 
   async run(context: BuilderContext): Promise<BuildResult> {
-    console.log('Setting up project...');
-
-    // copy project template
-    const projectUUID = uuidv4();
-
-    //frontend
-    const frontendTemplatePath = getTemplatePath('react-ts');
-    // backend
-    const backendTemplatePath = getTemplatePath('template-backend');
-    const backendProjectPath = await copyProjectTemplate(
-      backendTemplatePath,
-      projectUUID,
-    );
-
-    if (!backendProjectPath) {
-      return {
-        success: false,
-        error: new Error('Failed to copy project template'),
-      };
-    }
+    this.logger.log('Setting up project...');
 
     const result = {
       projectName: 'online shoping',
