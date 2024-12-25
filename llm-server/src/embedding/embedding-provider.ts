0b412b069f4da58a8733eb719afdcb828b20e18d
NarwhalChen <spixel.xi@gmail.com>
2024-12-25 00:02:17 -0800

    feat: adding embedding provider and openai-embedding provider

---
+++ llm-server/src/embedding/embedding-provider.ts
@@ -0,0 +1,10 @@
+import { Response } from 'express';
+import { GenerateMessageParams } from '../types';
+export abstract class EmbProvider {
+    abstract init(): Promise<void>;
+    abstract generateEmbResponse(
+        params: GenerateMessageParams,
+        res: Response,
+    ): Promise<void>;
+    abstract getEmbList(res: Response): Promise<void>;
+}
