d051a2cd57c8fbf250b2194ce9d29da9222ad3a1
pengyu <pengyuchen01@gmail.com>
2025-02-04 15:58:45 -0500

    rebuild authentication

---
+++ frontend/src/app/providers/ApolloProvider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+export default function ApolloWrapper({ children }: { children: React.ReactNode }) {
+  const [client, setClient] = useState<ApolloClient<any> | null>(null);
+
+  useEffect(() => {
+    const client = new ApolloClient({
+      uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:4000/graphql',
+      cache: new InMemoryCache(),
+      credentials: 'include',
+    });
+    setClient(client);
+  }, []);
+
+  if (!client) {
+    return null;
+  }
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
---
+++ frontend/src/components/ui/icons/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export const GoogleIcon = () => (
+  <Image
+    src="/images/google.png"
+    alt="Google"
+    width={20}
+    height={20}
+    className="h-5 w-5"
+  />
+)
+
+export const GitHubIcon = () => (
+  <Image
+    src="/images/github-icon.png"
+    alt="GitHub"
+    width={20}
+    height={20}
+    className="h-5 w-5"
+  />
+)
